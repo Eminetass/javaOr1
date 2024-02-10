c450ca7da9f7053294d403d717be9fb64f2dd80c
Emine Tas <eminetas126@gmail.com>
2024-02-10 17:56:08 +0300

    etkinlikOnerme.java

---
+++ src/etkinlikOnerme.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+// HAVA DURUMUNA GÖRE ETKİNLİK ÖNEREN PROGRAM
+public class etkinlikOnerme {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Derece: ");
+        int sicaklik = scan.nextInt();
+        String event = "";
+
+        if (sicaklik < 5) {
+            event = "Kayak ";
+        } else if (sicaklik < 15) {
+            event = "Tiyatro";
+        } else if (sicaklik < 25) {
+            event = "Mangal";
+        } else {
+            event = "Yüzme";
+        }
+
+        System.out.println("Önerilen Etkinlik: " + event);
+    }
+
+}
